be44f2c70cd6cc4779ea4236acddd5ccb0141dfa
muhammet58 <you@example.com>
2021-02-15 22:41:17 -0500

    update weather app

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 
-
+//define express
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/index.html");
-
-    
-
-    
-    
 });
 
 app.post("/", (req, res) =>{
@@ -29,15 +23,12 @@ app.post("/", (req, res) =>{
             const city = weatherData.name;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
             res.write("<h1 style='text-transform: capitalize;'>" + city +  " Temp: " + temp + " Celcius </br></br>  Weather: " + desc + "</h1>");
-            res.write("<img src='" + imageURL + "'>");
-            
+            res.write("<img src='" + imageURL + "'>");   
             res.send();
         });
     });
 });
-
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000.");
